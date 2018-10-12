f0a30d6b173de37cff178c5a4eb9d03043f3071d
Roderick Trice <rocky_trice@yahoo.com>
2018-10-12 12:58:22 -0400

    using Next.js  tooling and learning to Route

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+const Home = props => (
+    <div>
+    <p>Home</p>
+    <Link href="/Sell">
+        <a>Sell</a>
+    </Link>    
+    </div>
+);
+
+export default Home;
