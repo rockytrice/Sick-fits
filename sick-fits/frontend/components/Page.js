1c785666021376506cbe33fadba20ce931ae73a9
Roderick Trice <rocky_trice@yahoo.com>
2018-10-12 17:03:00 -0400

    added global css to the app using injectGlobal from styled-components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -13,17 +13,52 @@ const theme = {
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
-
+//  font color for page(comes from theme)===========================
 const StyledPage = styled.div`
 background:white;
 color:${props => props.theme.black}
 `;
+
+
+// max width for inner div===========================================
 const Inner = styled.div`
 max-width:${props => props.theme.maxWidth};
 margin: 0 auto;
 padding: 2rem;
 
 `;
+// global CSS========================================================
+injectGlobal`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight:normal;
+  font-style:normal;
+}}
+html {
+  box-sizing: border-box;
+  font-size: 10px;
+}
+*, *:before, *after {
+  box-sizing:inherit;
+
+}
+body {
+  padding:0;
+  margin: 0;
+  font-size: 1.5rem;
+  line-height: 2;
+  font-family: 'radnika_next';
+
+}
+a {
+  text-decoration: none;
+  color: ${theme.black};
+}
+`;
+
+
 export default class Page extends Component {
   render() {
     return (
