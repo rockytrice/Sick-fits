b9bdcc9eb424d94600f0b039452d6d31c3da2231
Roderick Trice <rocky_trice@yahoo.com>
2018-10-12 17:18:47 -0400

    added all the links that will be needed to the nav.

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -6,12 +6,22 @@ import NavStyles from "./styles/NavStyles";
 const Nav = () => (
    <NavStyles>
        {/* This is client-side navigation; the action takes place in the browser, without making a request to the server. */}  
-    <Link href="/">
-        <a>Home</a>
-    </Link> 
-    <Link href="/Sell">
-        <a>Sell</a>
-    </Link> 
+        <Link href="/items">
+        <a>Item</a>
+        </Link>
+        <Link href="/Sell">       
+         <a>Sell</a>
+        </Link>
+        <Link href="/signup">
+        <a>Signup</a>      
+        </Link>
+        <Link href="/orders">
+        <a>Orders</a>
+        </Link>
+        <Link href="/me">
+        <a>Account</a>
+        </Link>
+
    </NavStyles> 
 )
 
