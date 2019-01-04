85604f35dd358a043237117c829b30cd05b7176c
Roderick Trice <rocky_trice@yahoo.com>
2019-01-04 14:43:25 -0500

    exposed  the create_item_mutation function.. wrapped the entire form tag in a mutation component. took care of error handling as well as loading handling

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,89 +1,108 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import Form from "./styles/Form";
 import FormatMoney from "../lib/formatMoney";
 import gql from "graphql-tag";
+import Error from "./ErrorMessage";
 
 // query thats going to submit the data. so this is sort of like a function that is going to take in these variables and when it is called its going to run createItem which we specified in our schema on the back in and use the passed in variables that we noted with the $. Once it has been created all we want back is the Item's id.
 const CREATE_ITEM_MUTATUION = gql`
-    mutation CREATE_ITEM_MUTATUION(
-        $title: String!
-        $price: Int!
-        $description: String!
-        $image: String
-        $largeImage: String
+  mutation CREATE_ITEM_MUTATUION(
+    $title: String!
+    $price: Int!
+    $description: String!
+    $image: String
+    $largeImage: String
+  ) {
+    CreateItem(
+      title: $title
+      price: $price
+      description: $description
+      image: $image
+      largeImage: $largeImage
     ) {
-        CreateItem(
-        title: $title
-        price: $price
-        description: $description
-        image: $image
-        largeImage: $largeImage
-        ) {
-            id
-        }
+      id
     }
+  }
 `;
 
- class CreateItem extends Component {
-     state= {
-         title: "",
-         description: "",
-         image: "",
-         largeImage: "",
-         price: 0,
-     };
-    //  fuction so we can access 'this' so we can update the state inside of the form and actually type inside of the input fields
-     handleChange = (e) => {
-        //  making it so that this fuction can be reused for all of the inputs on this page ⬇️..
-         const{name, type, value } = e.target;
-        //  if this is a number we need to make sure its a proper number because any text that comes out of an input, even if its a type of number it will still out it as a string so we need to coerce it to a proper number
-         const val= type === "number" ? parseFloat(value) :
-         value;
-        //  console.log({name, type, value});
-        //  modified the setState to work for any input using computed property names and it will mirror that change to state
-       this.setState({[name]: val});         
-     }
+class CreateItem extends Component {
+  state = {
+    title: "",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
+  };
+  //  fuction so we can access 'this' so we can update the state inside of the form and actually type inside of the input fields
+  handleChange = e => {
+    //  making it so that this fuction can be reused for all of the inputs on this page ⬇️..
+    const { name, type, value } = e.target;
+    //  if this is a number we need to make sure its a proper number because any text that comes out of an input, even if its a type of number it will still out it as a string so we need to coerce it to a proper number
+    const val = type === "number" ? parseFloat(value) : value;
+    //  console.log({name, type, value});
+    //  modified the setState to work for any input using computed property names and it will mirror that change to state
+    this.setState({ [name]: val });
+  };
   render() {
     return (
-        // listening for the submit event and pushing it up to the server.
-      <Form onSubmit ={(e) => {
-          e.preventDefault();
-          console.log(this.state);
-      }} >
-        <fieldset>
-          <label htmlFor="title">
-              Title 
-              <input type="text" id="title" name="title"
-              placeholder="Title" required 
-              value = {this.state.title}
-              onChange={this.handleChange}
-              />
-              
-          </label>
-          <label htmlFor="price">
-              Price 
-              <input type="number" id="price" name="price"
-              placeholder="Price" required 
-              value = {this.state.price}
-              onChange={this.handleChange}
-              />
-              
-          </label>
-          <label htmlFor="description">
-              Description
-              <textarea  id="description" name="description"
-              placeholder="Enter a description" required 
-              value = {this.state.description}
-              onChange={this.handleChange}
-              />
-          </label>
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
-    )
+      //    exposing the create_item_mutation function.. wrap the entire form tag in a mutation component. so when this mutatuion fires, its going to take a copy of this.state and send all of those values for the ride.
+      <Mutation mutation={CREATE_ITEM_MUTATUION} variables={this.state}>
+        {/* just like query, the only child of a mutation or a query can be an actul function. Instead of taking a payload, it gives us the mutationfunction(createItem) and the payload. then we return everything from below ⬇️ 😰 */}
+        {(CreateItem, { loading, error }) => (
+          <Form
+            onSubmit={e => {
+              // listening for the submit event and pushing it up to the server.
+              e.preventDefault();
+              console.log(this.state);
+            }}
+          >
+            <Error error={error} />
+            {/* 👍 adding the loading will stop the user from being able to edit or submit the form again. so if the loading is true then the user will not be able to edit the form. the aria-busy will tell the user if the group of fields is busy or not.Apollo will flip this on and off automatically 😃*/}
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter a description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
   }
 }
 
 export default CreateItem;
-export  { CREATE_ITEM_MUTATUION };
+export { CREATE_ITEM_MUTATUION };
--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,62 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
-import Item from './Item';
+import Item from "./Item";
 
 // best practice to put all of your queries in caps. Works exactly like styled components as so we write our queries in a string and you tag in using "gql" inside of back tics!!
-const ALL_ITEMS_QUERY = gql `
-query ALL_ITEMS_QUERY {
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
     items {
-        id
-        title
-        price
-        description
-        image
-        largeImage
+      id
+      title
+      price
+      description
+      image
+      largeImage
     }
-}
+  }
 `;
 
 const Center = styled.div`
-  text-align:center;
+  text-align: center;
 `;
 const ItemsList = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap:60px;
+  grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
 `;
 
-
-
 // the way that we use the query above is something called a rendered prop.
 
- class Items extends Component {
+class Items extends Component {
   render() {
     return (
       <Center>
         <Query query={ALL_ITEMS_QUERY}>
-        {/* the only child of a query component is going to  be a function. this will give you either loading state, error or the actual list of items it self.  */}
-
-        {/* destructurng the values out of payload that i wanted into variables  */}
-            {({ data, error, loading })=> {
-              console.log(data);
-              if(loading){
-                return <p>Loading...</p>
-              }
-              if(error){
-                return <p>Error: {error.message}</p>
-              }
-              return <ItemsList>
-              {/* note.. anytime you are looping over and spitting out multiple elements as siblings and they are the same component you must also give it a key prop!!  */}
-                {data.items.map( item => <Item item={item} key={item.id} />)}
-            </ItemsList>
-          }}
-        </Query>
+          {" "}
+          {/* the only child of a query component is going to  be a function. this will give you either loading state, error or the actual list of items it self.  */}
+          {/* destructurng the values out of payload that i wanted into variables  */}{" "}
+          {({ data, error, loading }) => {
+            console.log(data);
+            if (loading) {
+              return <p> Loading... </p>;
+            }
+            if (error) {
+              return <p> Error: {error.message} </p>;
+            }
+            return (
+              <ItemsList>
+                {" "}
+                {/* note.. anytime you are looping over and spitting out multiple elements as siblings and they are the same component you must also give it a key prop!!  */}{" "}
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}{" "}
+              </ItemsList>
+            );
+          }}{" "}
+        </Query>{" "}
       </Center>
-    )
+    );
   }
 }
 
