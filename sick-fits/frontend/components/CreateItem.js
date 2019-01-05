9cc9fd5a1363d384e17a4c439b869dd71e3b1da6
Roderick Trice <rocky_trice@yahoo.com>
2019-01-04 23:49:52 -0500

    successfully uploaded image for item on the sell page. Also added preview so the user can see what image was uploaded.

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
+import Router from "next/router";
 import Form from "./styles/Form";
 import FormatMoney from "../lib/formatMoney";
 import gql from "graphql-tag";
@@ -44,6 +45,32 @@ class CreateItem extends Component {
     //  modified the setState to work for any input using computed property names and it will mirror that change to state
     this.setState({ [name]: val });
   };
+
+   uploadFile = async (e) => {
+    console.log("uploading file");
+    // pull files out of the selection
+    const files = e.target.files;
+    // form data api which will prep all of the data
+    const data = new FormData();
+
+    data.append("file", files[0]);
+    // upload preset.. arguement needed by cloudinary
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/rocky/image/upload", {
+      method: "POST",
+      body: data
+    });
+    // parse data that comes back
+    const file = await res.json();
+    console.log(file);
+    this.setState ({
+      image: file.secure_url,
+      // eager is a secondary transform that happens and its going to transform a larger version of that so we don't have to wait for both of the request to come back 
+      largerImage: file.eager[0].secure_url
+    })
+  };
+
   render() {
     return (
       //    exposing the create_item_mutation function.. wrap the entire form tag in a mutation component. so when this mutatuion fires, its going to take a copy of this.state and send all of those values for the ride.
@@ -52,15 +79,34 @@ class CreateItem extends Component {
         {(createItem, { loading, error }) => (
           <Form
             onSubmit={async e => {
-              // listening for the submit event and pushing it up to the server.
+              // stop form for submitting..listen for the submit event and push it up to the server.
               e.preventDefault();
+              // call the mutation
               const res = await createItem();
+              // change them to the single item page
               console.log(res);
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id},
+              });
             }}
           >
             <Error error={error} />
             {/* 👍 adding the loading will stop the user from being able to edit or submit the form again. so if the loading is true then the user will not be able to edit the form. the aria-busy will tell the user if the group of fields is busy or not.Apollo will flip this on and off automatically 😃*/}
             <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Title
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {/* showing the user a preview of what image was loaded */}
+                {this.state.image && <img src={this.state.image } alt="Upload Preview" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
