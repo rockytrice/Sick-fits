7d2638caea690ff449b0570d6b9e3b35b02ffef7
Roderick Trice <rocky_trice@yahoo.com>
2019-01-10 15:32:56 -0500

    wrote single item query  to get access to the item it self so we can populate the item in the text fields so the user can see what they are editing

--- sick-fits/frontend/components/UpdateItem.js
+++ sick-fits/frontend/components/UpdateItem.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import { Mutation } from "react-apollo";
+import { Mutation, Query } from "react-apollo";
 import Router from "next/router";
 import Form from "./styles/Form";
 import FormatMoney from "../lib/formatMoney";
 import gql from "graphql-tag";
 import Error from "./ErrorMessage";
+
+// this is where we get access to the item it self. all we know is the  db's id and we need to pull that from the db in order to populate the item so the user can see what they are editing
+const SINGLE_ITEM_QUERY = gql`
+query SINGLE_ITEM_QUERY($id. ID!) {
+  item(where: {id: $id }) {
+    id
+    title
+    description
+    price
+  }
+}
+`;
 
 // query thats going to submit the data. so this is sort of like a function that is going to take in these variables and when it is called its going to run createItem which we specified in our schema on the back in and use the passed in variables that we noted with the $. Once it has been created all we want back is the Item's id.
 const UPDATE_ITEM_MUTATION = gql`
@@ -42,67 +54,79 @@ class UpdateItem extends Component {
 
   render() {
     return (
-      //    exposing the UPDATE_ITEM_MUTATION function.. wrap the entire form tag in a mutation component. so when this mutatuion fires, its going to take a copy of this.state and send all of those values for the ride.
-      <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
-        {/* just like query, the only child of a mutation or a query can be an actul function. Instead of taking a payload, it gives us the mutationfunction(createItem) and the payload. then we return everything from below ⬇ 😰 */}
-        {(createItem, { loading, error }) => (
-          <Form
-            onSubmit={async e => {
-              // stop form for submitting..listen for the submit event and push it up to the server.
-              e.preventDefault();
-              // call the mutation
-              const res = await createItem();
-              // change them to the single item page
-              console.log(res);
-              Router.push({
-                pathname: "/item",
-                query: { id: res.data.createItem.id }
-              });
-            }}
-          >
-            <Error error={error} />
-            {/* 👍 adding the loading will stop the user from being able to edit or submit the form again. so if the loading is true then the user will not be able to edit the form. the aria-busy will tell the user if the group of fields is busy or not.Apollo will flip this on and off automatically 😃*/}
-            <fieldset disabled={loading} aria-busy={loading}>
-              <label htmlFor="title">
-                Title
-                <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  placeholder="Title"
-                  required
-                  value={this.state.title}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <label htmlFor="price">
-                Price
-                <input
-                  type="number"
-                  id="price"
-                  name="price"
-                  placeholder="Price"
-                  required
-                  value={this.state.price}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <label htmlFor="description">
-                Description
-                <textarea
-                  id="description"
-                  name="description"
-                  placeholder="Enter a description"
-                  required
-                  value={this.state.description}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <button type="submit">Submit</button>
-            </fieldset>
-          </Form>
-        )}
-      </Mutation>
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id
+        }}
+      >
+        {/* take the data that was returned from our single item query and put them into the input boxes but not tie it to state directly but show the user what they have and then if they change anything then put it into state!!😎  */}
+        {({ data, loading }) => {
+          return (
+            //    exposing the UPDATE_ITEM_MUTATION function.. wrap the entire form tag in a mutation component. so when this mutatuion fires, its going to take a copy of this.state and send all of those values for the ride.
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {/* just like query, the only child of a mutation or a query can be an actul function. Instead of taking a payload, it gives us the mutationfunction(createItem) and the payload. then we return everything from below ⬇ 😰 */}
+              {(createItem, { loading, error }) => (
+                <Form
+                  onSubmit={async e => {
+                    // stop form for submitting..listen for the submit event and push it up to the server.
+                    e.preventDefault();
+                    // call the mutation
+                    const res = await createItem();
+                    // change them to the single item page
+                    console.log(res);
+                    Router.push({
+                      pathname: "/item",
+                      query: { id: res.data.createItem.id }
+                    });
+                  }}
+                >
+                  <Error error={error} />
+                  {/* 👍 adding the loading will stop the user from being able to edit or submit the form again. so if the loading is true then the user will not be able to edit the form. the aria-busy will tell the user if the group of fields is busy or not.Apollo will flip this on and off automatically 😃*/}
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        value={this.state.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Enter a description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">Save Changes</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
     );
   }
 }
